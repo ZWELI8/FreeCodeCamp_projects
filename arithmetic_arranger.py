db009dde22a5d36b909900d46b44f2f251dbe4f7
ZWELI8 <zwelibanzin423@gmail.com>
2023-03-25 22:06:57 +0200

    Update arithmetic_arranger.py
    Some of the output are correct

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,23 +1,36 @@
-def arithmetic_arranger(problems):
+'''
+    * Name: Zwelibanzi Gumede
+    * Date:25 March 2023
+    * This is the program which do the sum of two numbers.
+    '''
+def arithmetic_arranger(problems, bool = False):
     operators = []
     first_int = []
     sec_int = []
     answer = []
-    count = 0
     int_len1= []
     int_len2= []
+    count = 0
+   
     space = 0
+    if (len(problems) > 5):
+        return "Too many problems."
     for prob in problems:
         operators.append(str(prob.split(" ")[1]))
-        first_int.append(int(prob.split(" ")[0]))
-        sec_int.append(int(prob.split(" ")[2]))
+        if((prob.split(" ")[0]).isdigit() and prob.split(" ")[2].isdigit()):
+            first_int.append(int(prob.split(" ")[0]))
+            sec_int.append(int(prob.split(" ")[2]))
+        else:
+            return "Numbers must only contain digits."
+        
+
         int_len1.append(len(prob.split(" ")[0]))
         int_len2.append(len(prob.split(" ")[2]))
 
 
     for sign in operators:
         if (sign != "+" and sign != "-"):
-            return "Operator must be '+' or '-'"
+            return "Operator must be '+' or '-'."
         elif(sign == '+'):
             answer.append(first_int[count]+sec_int[count])
             count+=1
@@ -26,8 +39,9 @@ def arithmetic_arranger(problems):
             count+=1
     
 
-    if (len(problems) > 5):
-        return "Too many problems"
+    for i in range(len(problems)):
+        if (len(str(first_int[i]))> 4 or len(str(sec_int[i]))>4):
+            return "Numbers cannot be more than four digits."
 
     arranged_problems = ''
     for i in range(len(problems)):
@@ -42,11 +56,52 @@ def arithmetic_arranger(problems):
     space = 0
     for j in range(len(problems)):
         if (j==0):
-            space = 2  
-        if(int_len1[j]>= int_len2[j]):
-            arranged_problems+=(operators[j]+" "+str(sec_int[j])).rjust(int_len1[j]+2)
+            if(int_len1[j]>= int_len2[j]):
+                arranged_problems+=(operators[j]+" "+abs(len(str(first_int[j]))-len(str(sec_int[j])))*" "+str(sec_int[j])).rjust(int_len1[j]+2)
+            else:
+                arranged_problems+=(operators[j]+" "+str(sec_int[j])).rjust(int_len2[j]+2)
         else:
-            arranged_problems+=(operators[j]+" "+str(sec_int[j])).rjust(int_len2[j]+2)
+            if(int_len1[j]>= int_len2[j]):
+                arranged_problems+=4*" "
+                arranged_problems+=(operators[j]+" "+abs(len(str(first_int[j]))-len(str(sec_int[j])))*" "+str(sec_int[j])).rjust(int_len1[j]+2)
+                
+            else:
+                arranged_problems+=4*" "
+                arranged_problems+=(operators[j]+" "+str(sec_int[j])).rjust(int_len2[j]+2)
+                
+    arranged_problems+='\n'
+    
+    for x in range(len(problems)):
+        if x==0:
+            if(int_len1[j]>= int_len2[j]):
+                arranged_problems+=2*"-"+int_len1[x]*"-"
+            else:
+                arranged_problems+=2*"-"+int_len2[x]*"-"
+        else:
+            arranged_problems+=4*" "
+            if(int_len1[x]>= int_len2[x]):
+               
+                arranged_problems+=2*"-"+int_len1[x]*"-"
+            else:
+                arranged_problems+=2*"-"+int_len2[x]*"-"
 
-    return arranged_problems
-print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
+
+
+    if bool != True:
+         return arranged_problems
+    else:
+        arranged_problems+='\n'
+        for y in range(len(problems)):
+            if(y>0):
+                space = 4
+            
+            if(int_len1[y]>= int_len2[y]):
+                arranged_problems +=str(answer[y]).rjust(int_len1[y]+2+space)
+            else:
+                arranged_problems +=str(answer[y]).rjust(int_len2[y]+2+space)
+        return arranged_problems
+
+    return ""
+    
+   
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"],True))
